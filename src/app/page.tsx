8ff2009e792840d91e3b8f8b33fe52b1f8e55935
gabrielelpidio <gabrieldeandradeleal@gmail.com>
2024-03-18 11:22:50 -0400

    feat: Added main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,31 @@ import Posts from "./_components/posts";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <SignedOut>
-        <Link href="/sign-up">Sign up</Link>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <main className="flex h-[100dvh]">
+      <aside className="flex w-72 flex-shrink-0 flex-grow-0 flex-col justify-between border-r border-gray-200 py-6 pl-4 pr-5">
+        <div className="flex">
+          <nav className="pl-4">
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/my-posts">My Posts</Link>
+              </li>
+            </ul>
+          </nav>
+          <hr className="divide-y" />
+        </div>
+
+        <div>
+          <SignedOut>
+            <Link href="/sign-up">Sign up</Link>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </aside>
       <section>
         <CreatePost />
         <Posts />
