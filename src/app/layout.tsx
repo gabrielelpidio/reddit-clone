3293aa6d8e56872e829a906297d06867bffa70e4
gabrielelpidio <gabrieldeandradeleal@gmail.com>
2024-03-18 13:47:30 -0400

    fix: incorrect language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`font-sans ${inter.variable}`}>
+        <body className={`font-[var(--font-sans)] ${inter.variable}`}>
           <TRPCReactProvider>{children}</TRPCReactProvider>
         </body>
       </html>
