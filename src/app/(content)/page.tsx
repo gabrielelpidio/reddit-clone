b5f83d7b82886c76e141b2cd6ce92cc279e86817
gabrielelpidio <gabrieldeandradeleal@gmail.com>
2024-03-18 22:15:50 -0400

    feat: Only show CreatePost if signed in

--- src/app/(content)/page.tsx
+++ src/app/(content)/page.tsx
@@ -1,13 +1,13 @@
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import Link from "next/link";
+import { SignedIn } from "@clerk/nextjs";
 import { CreatePost } from "../_components/create-post";
 import Posts from "../_components/posts";
-import NavbarLink from "../_components/navbar/link";
 
 export default async function Home() {
   return (
     <>
-      <CreatePost />
+      <SignedIn>
+        <CreatePost />
+      </SignedIn>
       <Posts />
     </>
   );
