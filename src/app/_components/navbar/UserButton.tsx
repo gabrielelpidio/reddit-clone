34172257354da65ff4ad0de5c75470045c02adde
gabrielelpidio <gabrieldeandradeleal@gmail.com>
2024-03-18 22:14:54 -0400

    feat: Add dropdown menu for user profile in navbar

--- src/app/_components/navbar/UserButton.tsx
+++ src/app/_components/navbar/UserButton.tsx
@@ -1,20 +1,56 @@
 "use client";
 import * as React from "react";
-import { useUser } from "@clerk/nextjs";
+import {
+  useUser,
+  UserProfile as UserProfileClerk,
+  useAuth,
+  useSession,
+} from "@clerk/nextjs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { User } from "@clerk/nextjs/server";
+import { LogOutIcon } from "lucide-react";
 
 const UserProfile = () => {
+  const [open, setOpen] = React.useState(false);
   const user = useUser();
+  const auth = useAuth();
   if (!user.isLoaded || !user.isSignedIn) return null;
   return (
-    <div className="flex items-center gap-4">
-      {/* eslint-disable-next-line @next/next/no-img-element*/}
-      <img
-        src={user.user?.imageUrl}
-        alt={user.user?.fullName ?? "User profile picture"}
-        className="size-8 rounded-full object-cover"
-      />
-      <span className="w-full truncate">{user.user?.fullName}</span>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        className="flex items-center gap-4 px-4 py-2 hover:bg-gray-50"
+        onClick={() => setOpen((o) => !o)}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element*/}
+        <img
+          src={user.user?.imageUrl}
+          alt={user.user?.fullName ?? "User profile picture"}
+          className="size-8 rounded-full object-cover"
+        />
+        <span className="w-full truncate">{user.user?.fullName}</span>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuItem onClick={() => auth.signOut()}>
+          <LogOutIcon className="mr-2 h-4 w-4" />
+
+          <span>Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
